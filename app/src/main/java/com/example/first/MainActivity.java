eb990eaf5b1e4d539d5dda0ed331a576f1eec4c5
Satyam Soni <42893127+SatyamSoni23@users.noreply.github.com>
2019-09-17 14:48:54 +0530

    base

--- app/src/main/java/com/example/first/MainActivity.java
+++ app/src/main/java/com/example/first/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.first;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText email, password;
+    Button signup;
+    public static String strPassword, strEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        signup = findViewById(R.id.signup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                strEmail = email.getText().toString();
+                strPassword = password.getText().toString();
+            }
+        });
     }
 }
